5d6f7954a8dead469d06e5ecf4d6f27a32994963
Davidovich Dmitry <42535493+dmitryDuk3@users.noreply.github.com>
2018-09-26 11:40:22 +0300

    up

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
 module.exports = function getZerosCount(number, base) {
   let k = base;// your implementation
-  let plains = [];
+  let array = [];
 
   for (let i = 2; i <= base;) {
     if (k % i == 0) {
-      if (isNaN(plains[i])) {
-        plains[i] = 0;
+      if (isNaN(array[i])) {
+        array[i] = 0;
       }
-      plains[i]++;
+      array[i]++;
       k = k / i;
     }
     else {
       i++;
     }
   }
-  let answer = [];
+  
+  let arrayTwo = [];
   for (let i = 0; i <= base; ++i) {
-    if (!isNaN(
-      plains[i])) {
+    if (!isNaN(array[i])) {
       let temp = i, res = 0;
       while (number / temp) {
         res = res + Math.floor(number / temp);
         temp = temp * i;
       }
-      answer.push(Math.floor(res /
-        plains[i]));
+      arrayTwo.push(Math.floor(res / array[i]));
     }
   }
-  return arrayMin(answer);
-}
-function arrayMin(array) {
+  return arrayMin(arrayTwo);
+  
+  function arrayMin(array) {
   return array.reduce((a, b) => Math.min(a, b));
+}
 }
