1aa0b0084e191332d74f9f71e4bc84a15c155fa9
Richard B <C:\Users\dmitr\AppData\Roaming\The Bat!>
2018-09-26 10:42:36 +0300

    one more time

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 module.exports = function getZerosCount(number, base) {
-  let amount = 0;// your implementation
-    let array = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127];
-    for(let i = 0; i < array.length; i++) {
-    while (base % array[i] === 0 && base / array[i] !== 1) {
-      base = base / array[i];
+  let k = base;// your implementation
+  let plains = [];
+
+  for (let i = 2; i <= base;) {
+    if (k % i == 0) {
+      if (isNaN(plains[i])) {
+        plains[i] = 0;
+      }
+      plains[i]++;
+      k = k / i;
+    }
+    else {
+      i++;
     }
   }
-  
-   while(number > base) {
-    amount += parseInt(number / base);
-    number = number / base;
-  } 
-  
-  return amount;
+  let answer = [];
+  for (let i = 0; i <= base; ++i) {
+    if (!isNaN(
+      plains[i])) {
+      let temp = i, res = 0;
+      while (number / temp) {
+        res = res + Math.floor(number / temp);
+        temp = temp * i;
+      }
+      answer.push(Math.floor(res /
+        plains[i]));
+    }
+  }
+  return arrayMin(answer);
+}
+function arrayMin(array) {
+  return array.reduce((a, b) => Math.min(a, b));
 }
